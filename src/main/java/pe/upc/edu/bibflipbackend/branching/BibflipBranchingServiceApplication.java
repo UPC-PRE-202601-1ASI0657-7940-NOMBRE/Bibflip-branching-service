f57c4b5bb10ac01973cf8724195639f62b0aa7e9
P1er0VL <u20211a620@upc.edu.pe>
2026-06-05 04:51:30 -0500

    chore: initial Spring Boot setup para branching service

---
+++ src/main/java/pe/upc/edu/bibflipbackend/branching/BibflipBranchingServiceApplication.java
@@ -0,0 +1,13 @@
+package pe.upc.edu.bibflipbackend.branching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibflipBranchingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibflipBranchingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/upc/edu/bibflipbackend/branching/BibflipBranchingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.upc.edu.bibflipbackend.branching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibflipBranchingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
